e322dba3dc4cfd41b387a45f5480a83aaf4ad5aa
Andrey Popazov <andrey.plagiatix@gmail.com>
2019-03-22 14:49:17 +0200

    Initial commit

---
+++ QUAM-TEST/QUAM-TEST/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QUAM-TEST
+//
+//  Created by sys-246 on 3/22/19.
+//  Copyright © 2019 sys-246. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
